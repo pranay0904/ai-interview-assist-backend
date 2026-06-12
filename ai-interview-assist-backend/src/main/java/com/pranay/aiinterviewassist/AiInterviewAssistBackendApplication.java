db07f096978f72ec9780827dbdd1a9eace02a752
pranay0904 <pranaymandela@gmail.com>
2026-06-13 02:48:23 +0530

    Created project structure and added dependencies

---
+++ ai-interview-assist-backend/src/main/java/com/pranay/aiinterviewassist/AiInterviewAssistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pranay.aiinterviewassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiInterviewAssistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiInterviewAssistBackendApplication.class, args);
+    }
+
+}
---
+++ ai-interview-assist-backend/src/test/java/com/pranay/aiinterviewassist/AiInterviewAssistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.aiinterviewassist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiInterviewAssistBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
